fcea6d1727a64bc38e14ea481011ec6e2d6f0b46
Misho Mihov <mishomihov41@gmail.com>
2025-04-04 01:07:11 +0300

    completed

---
+++ MessagefromSpace/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string demo = Console.ReadLine();
+            var a = int.TryParse(demo, out int result);
+
+            Console.WriteLine(a);
+            
+        }
+    }
+}
---
+++ MessagefromSpace/Main/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+/*
+AB[3CD]
+IF[2E]LG[5O]D
+*/
+namespace Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get input from the user
+            string input = Console.ReadLine();
+
+            //Extracting the blocks with brackets
+            string[] parts = input.Split(new char[]{'[' , ']'}, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            // StringBiulder for the final message
+            StringBuilder message = new();
+
+            // Method for decrypting the message
+            DecryptSpaceMessage(parts, message);
+
+            // Print the final message
+            Console.WriteLine(message.ToString().ToUpper());
+        }
+
+        private static void DecryptSpaceMessage(string[] parts, StringBuilder message)
+        {
+            // Loop through the parts of the message
+            for (int i = 0; i < parts.Length; i++)
+            {
+                // Get the current part
+                string part = parts[i];
+                string repeatCountAsString = String.Empty;
+
+                // Loop through the characters of the part
+                for (int j = 0; j < part.Length; j++)
+                {
+                    string current = part[j].ToString();
+
+                    // Check if character is a number
+                    bool isNumber = int.TryParse(current, out int result);
+                    if (isNumber)
+                    {
+                        repeatCountAsString += current;
+                        part = part.Remove(j, 1);
+                        j--;
+                    }
+                }
+
+                // Trying to parse the repeat count
+                int repeatCount;
+                var parsingSuccessful = int.TryParse(repeatCountAsString, out repeatCount);
+
+                // If parsing is successful, repeat the part
+                if (parsingSuccessful)
+                {
+                    for (int j = 0; j < repeatCount; j++)
+                    {
+                        message.Append(part);
+                    }
+                }
+                // If parsing is not successful, append the part
+                else
+                {
+                    message.Append(part);
+                }
+
+            }
+        }
+    }
+}
