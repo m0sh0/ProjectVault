d74ed3b33cde38d0a706898de94c88f204bf4c65
Misho Mihov <mishomihov41@gmail.com>
2025-05-13 18:21:49 +0300

    long break added

--- PomodoroTimer/PomodoroTimer/wwwroot/js/site.js
+++ PomodoroTimer/PomodoroTimer/wwwroot/js/site.js
@@ -1,11 +1,13 @@
 ﻿// variables for the timer
 let minutes = 0;
-let seconds = 10;
+let seconds = 3;
 let timer = null;
 
 // variables for the default time
-let defaultMinutes = 25;
-let defaultSeconds = 0;
+let defaultMinutes = 0;
+let defaultSeconds = 3;
+
+//let permissionGranted = notifyMe();
 
 // variables for the break
 let breakCount = 0;
@@ -20,50 +22,77 @@ document.getElementById("count-of-breaks").innerText = `#${breakCount}`;
 
 document.getElementById("time-for-break-or-work").innerText = "Time to focus!";
 
-// variable for the button
-let button = document.getElementById("start-button");
+// variable for the buttons
+const button = document.getElementById("start-button");
+const resetButton = document.getElementById("reset-button");
+
+// requesting permission for notifications
+window.addEventListener("DOMContentLoaded", () => {
+	Notification.requestPermission();
+});
 
 // function for the countdown of the timer
 function countDown() {
 
 	// decrement the time
-	if (seconds == 0) {
-		if (minutes == 0) {
+	if (seconds === 0) {
+		if (minutes === 0) {
 
 			// check for work mode or break mode
 			if (!isBreak) {
 
+                // notifying the user for the break
+                if (Notification.permission === "granted") {
+                    const notification = new Notification("Time for a break!", {
+                        body: "PomoMih"
+                    });
+
+                    notification.onshow = function () {
+                        setTimeout(() => {
+                            notification.close();
+                        }, 3000);
+                    }
+				}
+
+                // playing the break mode audio
+                breakModeAudio();
+
 				numberOfBreaks++;
 				breakCount++;
 
-				// playing the break mode audio
-				breakModeAudio();
+                // hiding the time adjust in break mode
+                document.getElementById("time-adjust").style.display = "none";
 
-				// switching to break mode
-				document.body.classList.remove("work-mode");
-				document.body.classList.add("break-mode");
-
-				// hiding the time adjust in break mode
-				document.getElementById("time-adjust").style.display = "none";
-				
-				// displaying the break count
-				document.getElementById("time-for-break-or-work").innerText = "Time for a break!";
+                // displaying the break count
+                //document.getElementById("time-for-break-or-work").innerText = "Time for a break!";
                 document.getElementById("count-of-breaks").innerText = `#${breakCount}`;
 
+                // clear the timer
+                clearInterval(timer);
+                timer = null;
 
-				// clear the timer
-				clearInterval(timer)
-				timer = null;
+				if (numberOfBreaks < 4) {
 
-				// check for the number of breaks
-				if (numberOfBreaks == 4) {
+					// switching to break mode
+					document.body.classList.remove("work-mode");
+					document.body.classList.add("break-mode");
 
-					startInterval(0, 10);
-					numberOfBreaks = 0;
-				}
-				else {
+                    document.getElementById("time-for-break-or-work").innerText = "Time for a break!";
 
                     startInterval(0, 5);
+                }
+
+				else if (numberOfBreaks === 4) {
+
+					// switching to long break mode
+					document.body.classList.remove("work-mode");
+					document.body.classList.add("long-break-mode");
+
+                    document.getElementById("time-for-break-or-work").innerText = "Time for a loooong break!";
+
+					startInterval(0, 10);
+
+					numberOfBreaks = 0;
 				}
 
 				isBreak = true;
@@ -81,10 +110,10 @@ function countDown() {
                 document.getElementById("time-for-break-or-work").innerText = "Time to focus!";
 
 
-				clearInterval(timer)
+                clearInterval(timer);
 				timer = null;
 
-				startInterval(0, 10);
+				startInterval(defaultMinutes, defaultSeconds);
 				isBreak = false;
 
 			}
@@ -118,6 +147,7 @@ function Start() {
 		return;
 	}
 
+	// starting the timer with the chosen time
 	startInterval(defaultMinutes, defaultSeconds);
 }
 function startInterval(minuteParam, secondsParam) {
@@ -128,9 +158,10 @@ function startInterval(minuteParam, secondsParam) {
 	
 	// removing event listeners
 	button.removeEventListener("click", Start);
+    removeButtonShadow("start-button");
 	button.addEventListener("click", Stop);
 
-	button.innerText = "Stop"
+    button.innerText = "Stop";
 
 	// starting the countdown
 	timer = setInterval(countDown, 1000);
@@ -143,11 +174,12 @@ function startInterval(minuteParam, secondsParam) {
 function Stop() {
 
 	// remove the event listener for the stop button
-	button.removeEventListener("click", Stop)
-	button.addEventListener("click", Start)
+	button.removeEventListener("click", Stop);
+    addButtonShadow("start-button");
+    button.addEventListener("click", Start);
 
 	// change the text of the button
-	button.innerText = "Start"
+    button.innerText = "Start";
 
 	// stop the timer
 	clearInterval(timer);
@@ -156,6 +188,17 @@ function Stop() {
 
 }
 
+//event listeners for the reset button
+resetButton.addEventListener("mousedown", () => {
+    resetButton.classList.add("no-shadow");
+
+});
+
+resetButton.addEventListener("mouseup", () => {
+    resetButton.classList.remove("no-shadow");
+
+});
+  
 // function to reset the timer
 function Reset() {
 
@@ -190,11 +233,11 @@ function changeTime(minuteParam, secondsParam) {
 	document.getElementById("timer").innerText = FormatTime(minutes, seconds);
 
 	//remove event listeners
-	button.removeEventListener("click", Stop)
-	button.addEventListener("click", Start)
+    button.removeEventListener("click", Stop);
+    button.addEventListener("click", Start);
 
 	// change the name of the button
-	button.innerText = "Start"
+    button.innerText = "Start";
 
 	// stop the timer
 	clearInterval(timer);
@@ -213,4 +256,32 @@ function breakModeAudio() {
     let audio = new Audio("/sounds/BreakModeChime.mp3");
     audio.play();
 }
+//
+function removeButtonShadow(buttonName) {
+
+	let button = document.getElementById(buttonName);
+	button.classList.add("no-shadow");
+}
+function addButtonShadow(buttonName) {
+
+    let button = document.getElementById(buttonName);
+    button.classList.remove("no-shadow");
+}
+
+// function for requesting permission for notifications
+async function allowNotifications() {
+
+    if (!("Notification" in window)) {
+		alert("This browser does not support desktop notification");
+        return;
+    }
+
+	if (Notification.permission !== "granted") {
+        await Notification.requestPermission();
+        
+    }
+ }
+
+
+
  
