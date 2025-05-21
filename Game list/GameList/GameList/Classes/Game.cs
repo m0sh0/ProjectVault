e1cdbeb6c7748e8f93fd4b6066990be3ca3315fd
Mihail Mihov <mishomihov41@gmail.com>
2025-05-21 14:23:26 +0300

    Added database connection

---
+++ Game list/GameList/GameList/Classes/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameList.Classes
+{
+    public class Game
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string genre { get; set; }
+        public string platform { get; set; }
+        public DateTime releasedate { get; set; }
+        public bool completed { get; set; }
+        public int rating { get; set; }
+    }
+}
--- Game list/GameList/GameList/MainWindow.xaml.cs
+++ Game list/GameList/GameList/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,14 +9,57 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GameList.Classes;
+using Npgsql;
 
 namespace GameList
 {
     public partial class MainWindow : Window
     {
+        private const string connectionString = "Host=localhost;" +
+                                                "Port=5432;" +
+                                                "Username=postgres;" +
+                                                "Password=PostgreMishoSQL;" +
+                                                "Database=games";
+
         public MainWindow()
         {
             InitializeComponent();
+            LoadGames();
+        }
+
+        // Method to load games from the database
+        private void LoadGames()
+        {
+            var games = new ObservableCollection<Game>();
+
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+                using (var cmd = new NpgsqlCommand("SELECT * FROM games", conn))
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        games.Add(new Game
+                        {
+                            id = reader.GetInt32(0),
+                            title = reader.GetString(1),
+                            genre = reader.GetString(2),
+                            platform = reader.GetString(3),
+                            releasedate = reader.GetDateTime(4),
+                            completed = reader.GetBoolean(5),
+                            rating = reader.GetInt32(6)
+
+                        });
+                    }
+                }
+                GamesDataGrid.ItemsSource = games;
+
+
+
+
+            }
         }
     }
 }
