e7664aa4f55eccc33e563a8cd55a508ab53daf50
Misho Mihov <mishomihov41@gmail.com>
2025-05-24 22:17:43 +0300

    added insert button

--- Game list/GameList/GameList/View/UserControl/InputField.xaml.cs
+++ Game list/GameList/GameList/View/UserControl/InputField.xaml.cs
@@ -22,10 +22,34 @@ namespace GameList.View.UserControl
             InitializeComponent();
         }
 
+        private string placeholder;
+
+        public string Placeholder
+        {
+            get { return placeholder; }
+            set
+            {
+                placeholder = value;
+                PlaceHolder.Text = placeholder;
+            }
+        }
+
         private void ButtonClear_Click(object sender, RoutedEventArgs e)
         {
             TxtInput.Clear();
             TxtInput.Focus();
+        }
+
+        private void TxtInput_TextChanged(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(TxtInput.Text))
+            {
+                PlaceHolder.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                PlaceHolder.Visibility = Visibility.Hidden;
+            }
         }
     }
 }
