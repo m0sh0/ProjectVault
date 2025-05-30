b77cd141d388f5d21768166148e5a38004e04aab
Misho Mihov <mishomihov41@gmail.com>
2025-05-30 21:22:48 +0300

    Update DataBaseHelper.cs

--- Game list/GameList/GameList/Classes/DataBaseHelper.cs
+++ Game list/GameList/GameList/Classes/DataBaseHelper.cs
@@ -1,28 +1,62 @@
-﻿using System;
+﻿using GameList;
+using GameList.Classes;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
-using GameList.Classes;
+using System.Windows.Controls;
 
 public static class DataBaseHelper
 {
 
     private static string _connectionstring = LoadConnectionString();
 
+    public static async Task<ObservableCollection<Game>> LoadGamesAsync()
+    {
+        ObservableCollection<Game> games = new();
 
+        // Open a connection to the database
+        using (NpgsqlConnection conn = new())
+        {
+            await conn.OpenAsync();
 
+            using (NpgsqlCommand cmd = new("SELECT * FROM games ORDER BY id", conn))
+            using (NpgsqlDataReader reader = cmd.ExecuteReader())
+            {
+                // Read the data from the database and populate the ObservableCollection
+                while (reader.Read())
+                {
+                    games.Add(new Game
+                    {
+                        id = reader.GetInt32(0),
+                        title = reader.GetString(1),
+                        genre = reader.GetString(2),
+                        platform = reader.GetString(3),
+                        releasedate = reader.GetDateTime(4),
+                        completed = reader.GetBoolean(5),
+                        rating = reader.GetInt32(6)
+
+                    });
+                }
+
+                return games;
+            }
+        }
+    }
 
     private static string LoadConnectionString()
-    {
-        IConfigurationRoot config = new ConfigurationBuilder()
-            .SetBasePath(AppContext.BaseDirectory)
-            .AddJsonFile("appsettings.json")
-            .Build();
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json")
+                .Build();
 
-        return config["ConnectionString"];
-    }
+            return config["ConnectionString"];
+        }
 }
 
 
