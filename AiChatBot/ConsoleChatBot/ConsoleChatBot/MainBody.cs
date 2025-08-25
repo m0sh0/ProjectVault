d8b2bbe7c9ba126f9e6fc588d4d94ebbf772934c
misho <mishomihov41@gmail.com>
2025-08-25 17:35:57 +0300

    Need to figure out how to find json path

--- AiChatBot/ConsoleChatBot/ConsoleChatBot/MainBody.cs
+++ AiChatBot/ConsoleChatBot/ConsoleChatBot/MainBody.cs
@@ -1,4 +1,5 @@
-﻿using ConsoleChatBot.Classes;
+﻿using System.Text;
+using ConsoleChatBot.Classes;
 
 namespace ConsoleChatBot
 {
@@ -6,8 +7,14 @@ namespace ConsoleChatBot
     {
         static void Main(string[] args)
         {
-            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Connections.json");
-            var content = File.ReadAllText(path);
+            var path = Directory.GetCurrentDirectory();
+            var path1 = AppDomain.CurrentDomain.BaseDirectory;
+            //var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"Connections.json");
+            //var path = "/home/misho/RiderProjects/ProjectVault/AiChatBot/ConsoleChatBot/ConsoleChatBot/JSON/Connections.json";
+            
+            var content = File.ReadAllText(path, Encoding.UTF8);
+
+            Console.WriteLine(content);
             
             
             ApiService.Get(ConnectionLoader.LoadConnection(0), "Hello!");
